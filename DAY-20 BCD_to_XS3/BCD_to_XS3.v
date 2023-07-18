8573e1857c2625ac6632b2d8f7afce855f430985
Navya Toluchuri <navyatoluchuri@gmail.com>
2023-07-18 08:26:41 +0530

    BCD to XS3

---
+++ DAY-20 BCD_to_XS3/BCD_to_XS3.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.07.2023 16:13:16
+// Design Name: 
+// Module Name: BCD_to_XS3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BCD_to_XS3(x,b);
+input [3:0]b;
+output reg [3:0]x;
+always@(b)
+begin
+    if(b>4'b1001)
+        x=4'bxxxx;
+    else
+        x=b+4'b0011;
+
+end
+endmodule
